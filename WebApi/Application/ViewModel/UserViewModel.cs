f1389b1609d7859f436de1fb32e3fe26c017c7ed
MurilloKelvin <murillokelvin.a@gmail.com>
2026-02-28 13:06:50 -0300

    fix(user): allow setting role and active in view model

--- WebApi/Application/ViewModel/UserViewModel.cs
+++ WebApi/Application/ViewModel/UserViewModel.cs
@@ -7,6 +7,7 @@
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
-
+        public string Role { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
     }
 }
